2bd670b78db49e87e3d6174c147ddb7b33f4845c
Jason <jasonzhansen@foxmail.com>
2020-09-18 16:58:49 +0800

    some fixes

--- source/main.py
+++ source/main.py
@@ -10,6 +10,7 @@ database = input("Please input your target database name\n")
 my_db = MyDataBase(host,user,password,database)
 result = my_db.data()
 
+# REMEMBER to download a webdriver according to your browser
 # You can specify a path on your own pc or server
 PATH = "E:\CS_learnig\Snake-Python-tutorial-\WebBot\chromedriver.exe"
 
